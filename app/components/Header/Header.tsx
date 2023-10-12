abdc22db6f4bb9dc01e897f3a9543bdd0120b6dd
Dong <dong211cloudinarytraining@gmail.com>
2023-10-12 15:56:26 +0700

    create draft header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import styles from "./header.module.css";
 
 
 function Header() {
   return (
-    <header className={styles.header}>Header</header>
+    <header className={styles.header}>
+      <Image src="images/logo.svg" alt="game logo" width={80} height={80} />
+      <div className={styles.score}>12</div>
+    </header>
   )
 }
 
